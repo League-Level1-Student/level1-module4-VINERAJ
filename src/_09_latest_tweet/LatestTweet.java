359790957fdfd9e994a31df49931307d6f328f42
VINERAJ <47723941+VINERAJ@users.noreply.github.com>
2020-08-23 11:26:33 -0700

    on pig latin
    :100:

--- src/_09_latest_tweet/LatestTweet.java
+++ src/_09_latest_tweet/LatestTweet.java
@@ -3,6 +3,7 @@ package _09_latest_tweet;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.management.Query;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -28,5 +29,25 @@ public void actionPerformed(ActionEvent e) {
 	if(buttonPressed == button) {
 		System.out.println("Tweet, tweet");
 	}
+}
+private String getLatestTweet(String searchingFor) {
+
+    Twitter twitter = new TwitterFactory().getInstance();
+
+    AccessToken accessToken = new AccessToken(
+        "2453751158-IVD2VGZsvwZiRKxNe3Gs2lMjg30nvSkV1xSuPFf",
+        "vBa5PjKfuMTK1LLBG51nCUI9r5d6Ph5cAHrS73spg6Nvu");
+    twitter.setOAuthConsumer("YqeZdD2hYxOKv4QOkmp0i2djN",
+        "6XUB1r8KXBvd8Pk9HHW3NgphMxHvHWBLAr5TihnckMU0ttyGST");
+    twitter.setOAuthAccessToken(accessToken);
+
+Query query = new Query(searchingFor);
+    try {
+        QueryResult result = twitter.search(query);
+        return result.getTweets().get(0).getText();
+    } catch (Exception e) {
+        System.err.print(e.getMessage());
+        return "What the heck is that?";
+    }
 }
 }
---
+++ src/_10_pig_latin/PigLatin.java
@@ -0,0 +1,27 @@
+package _10_pig_latin;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class PigLatin {
+JFrame frame = new JFrame();
+JPanel panel = new JPanel();
+JTextField text = new JTextField(5);
+JButton button = new JButton();
+JLabel label = new JLabel();
+
+
+public void run() {
+	frame.add(panel);
+	panel.add(text);
+	panel.add(button);
+	button.setText("Translate");
+	panel.add(label);
+	label.setText(" ");
+	frame.pack();
+	frame.setVisible(true);
+}
+}
