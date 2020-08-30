9e118d1de83de6b6a3a12478d8e63169137d3cbe
VINERAJ <47723941+VINERAJ@users.noreply.github.com>
2020-08-30 11:25:09 -0700

    On whackamole
    :100:

--- src/_10_pig_latin/PigLatin.java
+++ src/_10_pig_latin/PigLatin.java
@@ -1,17 +1,21 @@
 package _10_pig_latin;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class PigLatin {
+public class PigLatin implements ActionListener {
 JFrame frame = new JFrame();
 JPanel panel = new JPanel();
 JTextField text = new JTextField(5);
 JButton button = new JButton();
 JLabel label = new JLabel();
+PigLatinTranslator translator = new PigLatinTranslator();
 
 
 public void run() {
@@ -19,7 +23,18 @@ public void run() {
 	panel.add(text);
 	panel.add(button);
 	button.setText("Translate");
+	button.addActionListener(this);
 	panel.add(label);
 	frame.pack();
 	frame.setVisible(true);
+}
+
+
+@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+	String theText = text.getText();
+	String word = translator.translate(theText);
+	label.setText(word);
+}
 }
